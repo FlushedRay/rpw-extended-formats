4af38563361735f9278227e6710169b2826a8161
FlushedRay <53062189+FlushedRay@users.noreply.github.com>
2024-06-02 15:36:46 -0400

    Update DialogExport.java
    Add support for pack formats up to 50

--- src/net/mightypork/rpw/gui/windows/dialogs/DialogExport.java
+++ src/net/mightypork/rpw/gui/windows/dialogs/DialogExport.java
@@ -107,7 +107,7 @@ public class DialogExport extends RpwDialog {
 
         mcOptsCombo = new JComboBox(choices);
         mcOptsCombo.setSelectedIndex(Config.CHOICE_EXPORT_TO_MC);
-        packMeta = new JComboBox(new String[]{"1", "2", "3", "4"});
+        packMeta = new JComboBox(new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50"});
         packMeta.setSelectedIndex(SequenceExportProject.getPackMetaNumber() - 1);
         unZip = Gui.checkbox(false);
 
